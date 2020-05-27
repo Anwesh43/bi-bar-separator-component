88110980adb5cf5dc6d9052544e728649bf64153
Anwesh43 <anweshthecool0@gmail.com>
2020-05-27 06:35:20 +0530

    adding BiBarSeparatorPresentational component

---
+++ src/BiBarSeparatorPresentational.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useStyle} from './hooks'
+const Bar = ({i, w, h, scale}) => {
+    const {useBarStyle} = useStyle(scale, w, h)
+    return <div style = {useBarStyle(i)}>
+    </div>
+}
+const BiBarSeparatorPresentational = ({onClick, w, h, scale}) => {
+    return <div onClick = {onClick}>
+        {[0, 1].map(i => <Bar key = {`bibar_${i}`} i = {i} w = {w} h = {h} scale = {scale}/>)}
+    </div>
+}
+
+export default BiBarSeparatorPresentational
