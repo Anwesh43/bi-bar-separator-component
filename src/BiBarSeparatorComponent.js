7748f607d3f346ea1eeeab74c1ab4958b97c4491
Anwesh43 <anweshthecool0@gmail.com>
2020-05-27 06:38:00 +0530

    adding BiBarSeparatorComponent

---
+++ src/BiBarSeparatorComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from 'react'
+import BiBarSeparatorPresentational from './BiBarSeparatorPresentational'
+
+const BiBarSeparatorComponent = (props) => {
+    const {w, h} = useDimension()
+    const {scale, start} = useAnimatedScale(0.02 / 2, 20)
+    return <BiBarSeparatorPresentational scale = {scale} onClick = {start} w = {w} h = {h}>
+    </BiBarSeparatorPresentational>
+}
+
+export default BiBarSeparatorComponent
